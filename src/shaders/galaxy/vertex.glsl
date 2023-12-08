39b02c3384df56c6bbefe22716f1468ffe6d24ba
Pavel <pakhal1414@gmail.com>
2023-12-08 09:23:20 +0100

    fix deploy 12

--- src/shaders/galaxy/vertex.glsl
+++ src/shaders/galaxy/vertex.glsl
@@ -21,7 +21,7 @@ void main()
     angle += angleOffset;
     // modelPosition.x = uYpos;
     // modelPosition.x *= sin(angle) / 2.0 ;
-    modelPosition.x = sin(angle) / 4.0 * aScale ;
+    // modelPosition.x = sin(angle) / 4.0 * aScale ;
     modelPosition.y = uYpos * aScale / 1.5;
 
     // Randomness
